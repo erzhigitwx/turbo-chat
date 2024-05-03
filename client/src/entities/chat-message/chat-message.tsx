3dbecbac00c4d0c0a011ca65dd01638f4a9732e3
erzhigitwx <erjik.red@gmail.com>
2024-05-03 19:05:38 +0500

    new component added

--- client/src/entities/chat-message/chat-message.tsx
+++ client/src/entities/chat-message/chat-message.tsx
@@ -2,6 +2,9 @@ import { ChatMessageProps } from './chat-message.props'
 import { Message } from '@/shared/UI/message/message'
 import { useUnit } from 'effector-react'
 import { $user } from '@/app/model'
+import { formattedTime } from '@/shared/utils'
+import ToCheckImg from '@/assets/icons/toCheck.svg?react'
+import CheckedImg from '@/assets/icons/checked.svg?react'
 
 const ChatMessage = ({ message, ...rest }: ChatMessageProps) => {
   const user = useUnit($user)
@@ -9,6 +12,10 @@ const ChatMessage = ({ message, ...rest }: ChatMessageProps) => {
   return (
     <Message isOpponent={message.senderId !== user?.uid} {...rest}>
       <p>{message.content}</p>
+      <div>
+        <p>{formattedTime(message.createdAt)}</p>
+        {(message.status === 'send' && <ToCheckImg />) || <CheckedImg />}
+      </div>
     </Message>
   )
 }
