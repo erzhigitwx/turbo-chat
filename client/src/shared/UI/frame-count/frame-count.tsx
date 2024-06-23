8f76917490181805bdfee3d684d4574468f5e1f1
erzhigitwx <erjik.red@gmail.com>
2024-06-23 10:35:39 +0500

    added types, code refactor

--- client/src/shared/UI/frame-count/frame-count.tsx
+++ client/src/shared/UI/frame-count/frame-count.tsx
@@ -1,17 +1,21 @@
 import cl from "./frame-count.module.scss";
-import { ReactNode } from "react";
+import {ReactNode} from "react";
 
-const FrameCount = ({ children, count }: { children: ReactNode; count: string | number }) => {
-  return Number(count) ? (
-    <div className={cl.frameCount}>
-      <div className={cl.frameCountInner}>
-        <p>{count}</p>
-      </div>
-      {children}
-    </div>
-  ) : (
-    children
-  );
+const FrameCount = ({children, count}: { children: ReactNode; count: string | number }) => {
+    return Number(count) ? (
+        <div className={cl.frameCount}>
+            <div className={cl.frameCountInner}>
+                <p>{count}</p>
+            </div>
+            {children}
+        </div>
+    ) : (
+        <>
+            {
+                children
+            }
+        </>
+    );
 };
 
-export { FrameCount };
+export {FrameCount};
--- client/src/shared/hooks/use-user-data.ts
+++ client/src/shared/hooks/use-user-data.ts
@@ -3,8 +3,6 @@ import { UserContext } from '@/app/providers/user-provider'
 
 export const useUserData = () => {
   const context = useContext(UserContext)
-  if (context === null) {
-    throw new Error('useUserData must be used within a UserProvider');
-  }
+
   return context
 }
